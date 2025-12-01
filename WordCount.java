9c288093868b55be872064305e2cb55482c3134f
saniya116 <shaiksaniya6865@gmail.com>
2025-12-01 18:32:43 +0530

    Create WordCount.java

---
+++ WordCount.java
@@ -0,0 +1,7 @@
+public class WordCount {
+    public static void main(String[] args) {
+        String sentence = "Java programming is fun";
+        String[] words = sentence.split(" ");
+        System.out.println("Total number of words: " + words.length);
+    }
+}
