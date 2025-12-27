b9ab767875a204942c5c649af9624ba3d02fd242
saniya116 <shaiksaniya6865@gmail.com>
2025-12-27 20:52:18 +0530

    Create bestclosingtime.java

---
+++ bestclosingtime.java
@@ -0,0 +1,14 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        curPenalty = sum(c == "Y" for c in customers)
+        minPenalty = curPenalty
+        earliestHour = 0
+        for i, ch in enumerate(customers):
+            if ch == "Y":
+                curPenalty -= 1
+            else:
+                curPenalty += 1
+            if curPenalty < minPenalty:
+                minPenalty = curPenalty
+                earliestHour = i + 1
+    return earliestHour
